f863f88bb0c1297a1317535a1a5fdcdd6430dd55
Bruno Freitas <59490875+viribusegovis@users.noreply.github.com>
2025-01-12 00:47:57 +0000

    Added missing routers

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,1 +1,6 @@
+from app.routers.auth import router as auth_router
 from app.routers.users import router as users_router
+from app.routers.recipes import router as recipes_router
+from app.routers.ingredients import router as ingredients_router
+from app.routers.instructions import router as instructions_router
+
