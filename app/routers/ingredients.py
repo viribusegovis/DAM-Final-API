b3261a14c32b3fe2906232a4c2516870d0f9c0dc
Bruno Freitas <59490875+viribusegovis@users.noreply.github.com>
2025-01-13 16:15:31 +0000

    Added GET endpoint for getting Top used ingredients

--- app/routers/ingredients.py
+++ app/routers/ingredients.py
@@ -1,9 +1,10 @@
 from typing import List
-
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+from app.models import RecipeIngredient
 from app.models.ingredient import Ingredient
 from app.schemas.ingredient import IngredientResponse
 
@@ -16,9 +17,26 @@ def get_ingredients(db: Session = Depends(get_db)):
     return ingredients
 
 
+@router.get("/top", response_model=List[IngredientResponse])
+def get_top_ingredients(limit: int = 10, db: Session = Depends(get_db)):
+    top_ingredients = (
+        db.query(Ingredient.ingredient_id, Ingredient.name)
+        .join(RecipeIngredient)
+        .group_by(Ingredient.ingredient_id, Ingredient.name)
+        .order_by(func.count(RecipeIngredient.recipe_id).desc())
+        .limit(limit)
+        .all()
+    )
+
+    if not top_ingredients:
+        return []
+    return top_ingredients
+
+
 @router.get("/{ingredient_id}", response_model=IngredientResponse)
 def get_ingredient(ingredient_id: int, db: Session = Depends(get_db)):
-    ingredient = db.query(Ingredient).filter(Ingredient.ingredient_id == ingredient_id).first()
+    ingredient = db.query(Ingredient).filter(
+        Ingredient.ingredient_id == ingredient_id).first()
     if not ingredient:
         raise HTTPException(status_code=404, detail="Ingredient not found")
     return ingredient
--- main.py
+++ main.py
@@ -2,6 +2,4 @@ import uvicorn
 from app import create_app
 
 app = create_app()
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+uvicorn.run(app, host="0.0.0.0", port=8080)
