fd1f11df712766ef5e03649beb6dea1c5c6bd56d
Bruno Freitas <59490875+viribusegovis@users.noreply.github.com>
2025-01-13 15:25:11 +0000

    Replit changes

--- replit.nix
+++ replit.nix
@@ -1,5 +1,16 @@
 {pkgs}: {
   deps = [
+    pkgs.glibcLocales
+    pkgs.openssl
+    pkgs.gitFull
+    pkgs.pkg-config
+    pkgs.libffi
+    pkgs.gdb
+    pkgs.cacert
+    pkgs.rustc
+    pkgs.libiconv
+    pkgs.cargo
+    pkgs.zeromq
     pkgs.libxcrypt
     pkgs.unixODBC
   ];
